ee5e0d69e3bd315ecbee37d334e1fb8d1f5c632a
Rohit <stylishrb711@gmail.com>
2022-07-22 00:09:07 +0530

    chakra ui added

--- online-store/src/Components/Home.jsx
+++ online-store/src/Components/Home.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Slider } from "@chakra-ui/react";
 import { useEffect } from "react";
-import axios from 'axios';
-
+import axios from "axios";
+import CaptionCarousel from "./HeroPart.jsx";
+import ProCard from "./ProCard.jsx";
 const Home = () => {
   const [data, setData] = useState([]);
 
@@ -11,18 +12,30 @@ const Home = () => {
   }, []);
 
   const getData = async () => {
-    const res = await axios.get("https://fakestoreapi.com/products")
+    const res = await axios.get("https://fakestoreapi.com/products");
+    const data = res.data;
+    setData(data);
   };
   return (
     <div>
       <h1>Home</h1>
-      <ButtonGroup variant="solid" spacing="3">
-        <Button colorScheme="teal">All</Button>
-        <Button colorScheme="teal">Men's</Button>
-        <Button colorScheme="teal">Women's</Button>
-        <Button colorScheme="teal">Jewelery</Button>
-        <Button colorScheme="teal">Electronics</Button>
-      </ButtonGroup>
+      <div className="banner-slider">
+        <CaptionCarousel />
+      </div>
+
+      <div className="btn-div">
+        <ButtonGroup variant="solid" spacing="3">
+          <Button colorScheme="teal">All</Button>
+          <Button colorScheme="teal">Men's</Button>
+          <Button colorScheme="teal">Women's</Button>
+          <Button colorScheme="teal">Jewelery</Button>
+          <Button colorScheme="teal">Electronics</Button>
+        </ButtonGroup>
+      </div>
+
+      <div className="product-div">
+        <ProCard data={data} />
+      </div>
     </div>
   );
 };
