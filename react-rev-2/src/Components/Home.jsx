8c3d4029528de22256d70ab7de822f59ab2ba7f6
Rohit <stylishrb711@gmail.com>
2022-07-31 19:43:10 +0530

    data fetched

--- react-rev-2/src/Components/Home.jsx
+++ react-rev-2/src/Components/Home.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import { useSelector } from "react-redux";
 import Product from "./Product";
 
 const Home = () => {
+  const API = "https://fakestoreapi.com/products";
   const products = useSelector((state) => state);
-  console.log(products);
+
+  const getData = async () => {
+    try {
+      const res = await axios.get(API);
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div>
       <h1>All Products</h1>
--- react-rev-2/src/Components/Product.jsx
+++ react-rev-2/src/Components/Product.jsx
@@ -1,18 +1,30 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Product = () => {
+  const products = useSelector((state) => state.productReducer.products);
+  console.log(products);
   return (
     <div>
       <div className="card" style={{ width: "18rem" }}>
-        <img className="card-img-top" src="..." alt="Card image cap" />
+        <img
+          className="card-img-top pro-img"
+          src={products[0].image}
+          alt="Card image"
+        />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
+          <h5 className="card-title">{products[0].title}</h5>
+          <p className="card-text">{products[0].description}</p>
+          <p className="card-text">{products[0].category}</p>
+          <div className="row">
+            <div className="col-6">{products[0].price}</div>
+            <div className="col-6">{products[0].rating.rate}</div>
+          </div>
           <a href="#" className="btn btn-primary">
-            Go somewhere
+            Add to cart
+          </a>
+          <a href="#" className="btn btn-primary mx-2">
+            Buy Now
           </a>
         </div>
       </div>
