b5bdc0caeedfb3fb93aaa9a5ac74507161ab092f
Rohit <stylishrb711@gmail.com>
2022-07-31 14:07:15 +0530

    redux removed

--- react-rev-2/src/main.jsx
+++ react-rev-2/src/main.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Provider as ReactProvider } from "react-redux";
-import store from "./Redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ReactProvider store={store}>
-      <App />
-    </ReactProvider>
+    <App />
   </React.StrictMode>
 );
